01d67dd2fbf275dcbaacd31d17b28a02884f4d52
rebsaa <82534721+rebsaa@users.noreply.github.com>
2021-12-10 15:46:22 +0100

    Update Ortho_output.sh

--- Ortho_output.sh
+++ Ortho_output.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 #Determine the longest peptide sequence per orthogroup, run in OrthoFinder/$SPECIES/INPUT/OrthoFinder/Results*/Orthogroup_Sequences
 
-for FILE in *fa; do 
-awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" }END { printf "%s", n }' $FILE | paste - - | awk '{print $0 "\t" length($2)}'| sort -Vrk 3 | head -n 1 | cut -f1 >> longest_peptides.txt
+SPECIES=H.glaber
+>$SPECIES/$SPECIES\_longest_peptide_per_orthogroup.pep
+
+for FILE in $(find . -wholename './H.glaber/INPUT/OrthoFinder/*/Orthogroup_Sequences/*fa'); do 
+awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" }END { printf "%s", n }' $FILE | paste - - | awk '{print $0 "\t" length($2)}'| sort -Vrk 3 | head -n 1 | cut -f1 >> $SPECIES\/longest_peptide_per_orthogroup.pep
+ 
 done
+
+for SAMPLE in $(cut -d '_' -f 1-3 $SPECIES\/$SPECIES\_longest_peptide_per_orthogroup.pep | sed 's/>//g' | sort -u); do
+grep -wf $SPECIES\/$SPECIES\_longest_peptide_per_orthogroup.pep <(cat $SPECIES\/$SAMPLE\.fasta | paste - -) | tr '\t' '\n' > $SPECIES\/$SAMPLE\_longest_transcript_per_orthogroup.fasta
+done
+
+cat $SPECIES\/*longest_transcript_per_orthogroup.fasta > $SPECIES\/$SPECIES\_reference_transcript.fasta
