0714ef5f32e409f23a874a085dc736dbf456ffc6
rebsaa <82534721+rebsaa@users.noreply.github.com>
2021-12-16 11:25:06 +0100

    Update Ortho_prep.sh

--- Ortho_prep.sh
+++ Ortho_prep.sh
@@ -11,7 +11,7 @@ for FILE in $(find ../Results/Trinity/$SPECIES -wholename '*/Trinity.fasta'); do
 	##Extract the id of the isoform with the longest cds per gene
 	> $SPECIES\/$SAMPLE\_longest_prediction.txt
 	for GENE in $(grep '>' $CDS | cut -d ' ' -f1 | sed 's/_i[0-9].*//g;'| sort -u); do
-	grep '>' $CDS | grep $GENE |cut -d ' ' -f 1,2 | sort -V -r -k2 | head -n 1 | cut -d ' ' -f1 >> $SPECIES\/$SAMPLE\_longest_prediction.txt
+	grep '>' $CDS | grep $GENE |cut -d ' ' -f 1,6 | sort -V -r -k2 | head -n 1 | cut -d ' ' -f1 >> $SPECIES\/$SAMPLE\_longest_prediction.txt
 	done
 
 	cat $FILE | tr '\n' '\t' | sed 's/>/\n>/g' | grep -wf <(sed 's/\.p[0-9]*//g' $SPECIES\/$SAMPLE\_longest_prediction.txt) | tr '\t' '\n' | sed "/^\s*$/d;s/^>/>${SAMPLE}\_/g" | cut -d ' ' -f 1 > $SPECIES\/$SAMPLE.fasta
